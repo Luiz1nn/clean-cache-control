4794e145cd0bdd7cbc3996e81ffa2d6671c77dcb
Luis Fernando <luisfernandobr2003@gmail.com>
2022-03-18 11:47:39 -0300

    change Message with Action

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -4,37 +4,37 @@ import { CacheStore } from '@/data/protocols/cache';
 export class CacheStoreSpy implements CacheStore {
     deleteKey: string;
     insertKey: string;
-    messages: Array<CacheStoreSpy.Message> = [];
+    actions: Array<CacheStoreSpy.Action> = [];
     insertValues: Array<SavePurchases.Params> = [];
 
     delete(key: string): void {
-        this.messages.push(CacheStoreSpy.Message.delete);
+        this.actions.push(CacheStoreSpy.Action.delete);
         this.deleteKey = key;
     }
 
     insert(key: string, value: any): void {
-        this.messages.push(CacheStoreSpy.Message.insert);
+        this.actions.push(CacheStoreSpy.Action.insert);
         this.insertKey = key;
         this.insertValues = value;
     }
 
     simulateDeleteError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'delete').mockImplementationOnce(() => {
-            this.messages.push(CacheStoreSpy.Message.delete);
+            this.actions.push(CacheStoreSpy.Action.delete);
             throw new Error();
         });
     }
 
     simulateInsertError(): void {
         jest.spyOn(CacheStoreSpy.prototype, 'insert').mockImplementationOnce(() => {
-            this.messages.push(CacheStoreSpy.Message.insert);
+            this.actions.push(CacheStoreSpy.Action.insert);
             throw new Error();
         });
     }
 }
 
 export namespace CacheStoreSpy {
-    export enum Message {
+    export enum Action {
         delete,
         insert
     }
