7054bb36fa0b572ae94adfaaeb29d06693d5ddc4
Luis Fernando <luisfernandobr2003@gmail.com>
2022-03-18 16:19:53 -0300

    rename var to give more context

--- src/data/usecases/load-purchases/local-load-purchases.ts
+++ src/data/usecases/load-purchases/local-load-purchases.ts
@@ -5,11 +5,11 @@ export class LocalLoadPurchases implements SavePurchases, LoadPurchases {
 
     private readonly key = 'purchases';
 
-    constructor(private readonly cacheStore: CacheStore, private readonly timestamp: Date) { }
+    constructor(private readonly cacheStore: CacheStore, private readonly currentDate: Date) { }
 
     async save(purchases: Array<SavePurchases.Params>): Promise<void> {
         this.cacheStore.replace(this.key, {
-            timestamp: this.timestamp,
+            timestamp: this.currentDate,
             value: purchases
         });
     }
