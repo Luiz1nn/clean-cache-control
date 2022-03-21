6fbe3401b7ee5329edf59087bb6d1e21d9b3dca8
Luis Fernando <luisfernandobr2003@gmail.com>
2022-03-21 08:23:07 -0300

    extract cache max age in a variable

--- src/data/tests/mock-cache.ts
+++ src/data/tests/mock-cache.ts
@@ -1,9 +1,11 @@
 import { SavePurchases } from '@/domain/usecases';
 import { CacheStore } from '@/data/protocols/cache';
 
+const maxAgeInDays = 3;
+
 export const getCacheExpirationDate = (timestamp: Date): Date => {
     const maxCacheAge = new Date(timestamp);
-    maxCacheAge.setDate(maxCacheAge.getDate() - 3);
+    maxCacheAge.setDate(maxCacheAge.getDate() - maxAgeInDays);
     return maxCacheAge;
 }
 
